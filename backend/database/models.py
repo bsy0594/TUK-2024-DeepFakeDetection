e20118c60d752a183156db22d8ee461a2101e646
Yechan <baoro0812@gmail.com>
2025-02-23 13:51:33 +0900

    video, frame_prediction 테이블 추가

--- backend/database/models.py
+++ backend/database/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime, F
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, relationship
 from sqlalchemy.dialects.postgresql import UUID
-import ulid
+from uuid_extensions import uuid7
 import datetime
 
 class Base(DeclarativeBase, AsyncAttrs):
@@ -11,24 +11,25 @@ class Base(DeclarativeBase, AsyncAttrs):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=ulid.new)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid7())
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
 
     # 관계 설정
-    videos = relationship("Video", back_populates="user", cascade="all, delete-orphan")
+    # videos = relationship("Video", back_populates="user", cascade="all, delete-orphan") - 로그인 기능을 구현하지 않아 비활성화
 
 class Video(Base):
     __tablename__ = "videos"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=ulid.new)  # 비디오 고유 ID
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)  # 외래 키 (User 테이블의 id 참조)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid7())  # 비디오 고유 ID
+    # user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)  # 외래 키 (User 테이블의 id 참조) - 지금은 로그인 기능을 구현하지 않아 비활성화
     is_deepfake = Column(Boolean, nullable=False)  # 딥페이크 여부 (True/False)
-    created_at = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))  # 업로드 시간
+    model = Column(String, nullable=False)  # 사용된 모델
+    # created_at = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))  # 업로드 시간 - 지금은 오류가 발생하여 비활성화
 
     # 관계 설정
-    user = relationship("User", back_populates="videos")
+    # user = relationship("User", back_populates="videos") - 로그인 기능을 구현하지 않아 비활성화
     frames = relationship("FramePrediction", back_populates="video", cascade="all, delete-orphan")
 
 class FramePrediction(Base):
