215b7e47164ec3e3a8e22450bc36b88a19c74340
bsy0594 <bsy0594@tukorea.ac.kr>
2025-02-24 22:24:06 +0900

    웹 배포를 위한 config.yml 파일 경로 수정

--- frontend/sidebar.py
+++ frontend/sidebar.py
@@ -5,7 +5,7 @@ from yaml.loader import SafeLoader
 
 # Login
 def sidebar():
-    with open("./config.yaml") as file:
+    with open("./frontend/config.yaml") as file:
         config = yaml.load(file, Loader=SafeLoader)
 
     # Create authentication object
