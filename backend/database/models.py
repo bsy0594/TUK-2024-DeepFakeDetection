bf0d798e4f86b40f60a6a4f7ee57dd0acf0f88fa
bsy0594 <bsy0594@tukorea.ac.kr>
2025-02-24 13:39:03 +0900

    서버 관련 파일 업데이트

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,6 +1,9 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime, Float, PrimaryKeyConstraint
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy.dialects.postgresql import UUID
+from uuid_extensions import uuid7
+import datetime
 
 class Base(DeclarativeBase, AsyncAttrs):
     pass
@@ -8,7 +11,36 @@ class Base(DeclarativeBase, AsyncAttrs):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid7())
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
+
+    # 관계 설정
+    # videos = relationship("Video", back_populates="user", cascade="all, delete-orphan") - 로그인 기능을 구현하지 않아 비활성화
+
+class Video(Base):
+    __tablename__ = "videos"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid7())  # 비디오 고유 ID
+    # user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)  # 외래 키 (User 테이블의 id 참조) - 지금은 로그인 기능을 구현하지 않아 비활성화
+    is_deepfake = Column(Boolean, nullable=False)  # 딥페이크 여부 (True/False)
+    model = Column(String, nullable=False)  # 사용된 모델
+    # created_at = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))  # 업로드 시간 - 지금은 오류가 발생하여 비활성화
+
+    # 관계 설정
+    # user = relationship("User", back_populates="videos") - 로그인 기능을 구현하지 않아 비활성화
+    frames = relationship("FramePrediction", back_populates="video", cascade="all, delete-orphan")
+
+class FramePrediction(Base):
+    __tablename__ = "frame_predictions"
+
+    # id = Column(UUID(as_uuid=True), primary_key=True, default=ulid.new, index=True)  # 프레임 예측 ID
+    video_id = Column(UUID(as_uuid=True), ForeignKey("videos.id"), nullable=False, index=True)  # 영상 ID (UUID)
+    frame_number = Column(Integer, nullable=False, index=True)  # 프레임 번호
+    deepfake_probability = Column(Float, nullable=False)  # 딥페이크 확률 (0~1)
+
+    __table_args__ = (PrimaryKeyConstraint("video_id", "frame_number"),)
+
+    # 관계 설정
+    video = relationship("Video", back_populates="frames")
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI, Depends, UploadFile, File
+from fastapi import FastAPI, Depends, UploadFile, File, Form
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import models, database, schemas, crud
 from contextlib import asynccontextmanager
+from uuid_extensions import uuid7str
 import os
 import uuid
 import shutil
 import random
-import uvicorn
+from ml.process_video import extract_frames
+from ml.predict_deepfake_model import process_all_frames
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # 애플리케이션 시작 시 데이터베이스 테이블 생성
 async def init_db():
@@ -24,8 +28,21 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    r"https://tuk-2024-deepfakedetection.streamlit.app",  # 프론트 배포 도메인
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 허용할 origin 목록
+    allow_credentials=True,  # 인증 정보 포함 허용 (예: 쿠키, Authorization 헤더)
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용 (GET, POST, PUT 등)
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
+
 IMAGE_DIR = "images"
 VIDEO_DIR = "videos"
+# VIDEO_DIR = "images"
 
 os.makedirs(IMAGE_DIR, exist_ok=True)  # 디렉토리가 없으면 생성
 os.makedirs(VIDEO_DIR, exist_ok=True)
@@ -43,35 +60,44 @@ async def readUsers(skip: int = 0, limit: int = 10, db: AsyncSession = Depends(d
     return await crud.get_users(db, skip=skip, limit=limit)
 
 @app.post("/video/")
-async def postVideo(file: UploadFile = File(...)):
+async def postVideo(file: UploadFile = File(...), model: str = Form(...), db: AsyncSession = Depends(database.get_db)):
     """클라이언트가 업로드한 동영상을 서버에 저장하고, 저장된 URL을 반환"""
-    unique_id = str(uuid.uuid4())  # 파일명 충돌 방지를 위한 UUID 생성
-    file_extension = file.filename.split(".")[-1]  # 확장자 추출
-    filename = f"{unique_id}.{file_extension}"  # 고유한 파일명 생성
-    file_path = os.path.join(VIDEO_DIR, filename)  # 저장할 경로
+    # UUID 생성 및 디렉토리 생성
+    video_id = uuid7str()  # 파일명 충돌 방지를 위한 UUID 생성
+    video_directory = os.path.join(VIDEO_DIR, video_id)  # 저장할 디렉토리 경로
+    os.makedirs(video_directory, exist_ok=True)  # 디렉토리가 없으면 생성
 
     # # 파일 저장
-    # with open(file_path, "wb") as buffer:
-    #     shutil.copyfileobj(file.file, buffer)
+    # file_extension = file.filename.split(".")[-1]  # 확장자 추출
+    # # filename = f"{video_id}.{file_extension}"  # 고유한 파일명 생성
+    video_filename = file.filename
+    video_file_path = os.path.join(video_directory, video_filename)  # 저장할 경로
 
-    # file_url = f"/static/{filename}"  # 저장된 파일의 URL 생성
+    with open(video_file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
 
-    # return {"video_id": unique_id, "video_url": file_url}
+    # 파일 저장
+    image_directory = os.path.join(IMAGE_DIR, video_id)
+    original_image_directory = os.path.join(image_directory, "original")
+    gradcam_image_directory = os.path.join(image_directory, "gradcam")
+    os.makedirs(original_image_directory, exist_ok=True)
+    os.makedirs(gradcam_image_directory, exist_ok=True)
+    extract_frames(video_file_path, original_image_directory)
+
+    # 예측
+    predictions = process_all_frames(image_directory, use_gradcam=True)
+
+    # DB에 저장
+    video = models.Video(id=video_id, is_deepfake=random.choice([True, False]), model=model)
+    db.add(video)
+    await db.commit()
 
     # 로컬에 있는 이미지 파일을 URL로 변환하여 반환
-    image_files = os.listdir(IMAGE_DIR)
-    
-    # 이미지 파일들에 대한 URL 생성
+    # image_files = os.listdir(IMAGE_DIR)
+    image_files = os.listdir(image_directory)
     image_urls = [
-        {"image_id": str(uuid.uuid4()), "image_url": f"/static/{filename}", "prediction": random.random()}
-        for filename in image_files
-    ]
-    image_urls = [
-        {"frame_index": index, "image_url": f"/static/{filename}", "prediction": random.random()}
+        {"frame_index": index, "original_image": f"/static/{video_id}/original/{filename}", "gradcam_image": f"/static/{video_id}/gradcam/{filename}", "prediction": predictions[index]}
         for index, filename in enumerate(image_files)
     ]
     
-    return {"images": image_urls}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    return {"model": model, "images": image_urls}
